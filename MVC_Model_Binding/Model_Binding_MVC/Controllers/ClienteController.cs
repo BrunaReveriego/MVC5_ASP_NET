564f589243af3168694b3b6b5e47442bd4e5462d
Bruna Reveriego <bruna.reveriego91@gmail.com>
2020-10-27 17:31:37 -0300

    primeiro commit

---
+++ MVC_Model_Binding/Model_Binding_MVC/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model_Binding_MVC.Models;
+
+namespace Model_Binding_MVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Cliente cliente)
+        {
+
+            //model binding vai criar uma instância de Cliente
+            return View("ExibirDados", cliente);
+        }
+
+        public ActionResult ExibirDados(Cliente cliente)
+        {
+            return View(cliente);
+        }
+
+    }
+}
---
+++ MVC_Model_Binding/Model_Binding_MVC/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Model_Binding_MVC.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+
+
+
+
+    }
+}
